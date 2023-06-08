7aeb9d0da5a1243aa7898cab340e05b841f48c32
James Moreau <43487194+JamesMoreau@users.noreply.github.com>
2023-06-08 17:00:26 -0400

    .

--- main.go
+++ main.go
@@ -16,17 +16,6 @@ var (
 	special_characters = "!\"#$%&'*+,./:;=?@\\^`|~";
 )
 
-func main() {
-	pass, err := generate_password(24, true, true, true, true);
-	if err != nil {
-		fmt.Println("Hello, Sailor!");
-		return;
-	}
-
-	out := fmt.Sprintf("pass: %s", pass);
-	fmt.Println(out);
-}
-
 func generate_password(length int, include_uppercase_letters bool, include_digits bool, include_special_characters bool, exclude_ambiguous_characters bool) (string, error) {
 	if length < 1 {
 		e := "password must be at least 1 character long";
@@ -37,16 +26,23 @@ func generate_password(length int, include_uppercase_letters bool, include_digit
 	// Get the available characters depending on params.
 
 	available_characters := lowercase_letters;
-	if include_digits { available_characters += uppercase_letters };
-	if include_digits { available_characters += digits; }
-	if include_special_characters { available_characters += special_characters; }
+	
+	if include_digits { 
+		available_characters += uppercase_letters 
+	}
+	
+	if include_digits { 
+		available_characters += digits; 
+	}
+
+	if include_special_characters { 
+		available_characters += special_characters; 
+	}
 
 	// Remove the characters that may produce and "ambiguous" / hard-to-read passwords.
 	if exclude_ambiguous_characters {
-		for pos, char := range available_characters {
-			if strings.ContainsRune(ambiguous_characters, char) {
-				available_characters = removeCharAtIndex(available_characters, pos);
-			}
+		for _, char := range ambiguous_characters {
+			available_characters = strings.ReplaceAll(available_characters, string(char), "");
 		}
 	}
 
@@ -82,9 +78,12 @@ func report_error(error_message string) {
 	fmt.Println(string(color_red) + "Keyed Error: " + error_message + "." + string(color_reset));
 }
 
-func removeCharAtIndex(str string, index int) string {
-	if index < 0 || index >= len(str) {
-		return str // Invalid index, return the original string
+func main() {
+	pass, err := generate_password(24, true, true, true, true);
+	if err != nil {
+		return;
 	}
-	return str[:index] + str[index+1:]
+
+	out := fmt.Sprintf("password is: %s", pass);
+	fmt.Println(out);
 }
