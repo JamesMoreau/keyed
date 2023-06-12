ee2b578d9eb4555d80ee9b311533ddf6641c5b99
James Moreau <43487194+JamesMoreau@users.noreply.github.com>
2023-06-12 02:07:35 -0400

    ....

--- main.go
+++ main.go
@@ -11,9 +11,17 @@ type keyed struct {
 }
 
 func (k *keyed) Render() app.UI {
-	return app.Div().
-		Body(
+	return app.Div().Class("my-container").ID("parent").Body(
 			app.H1().Class("title").Text("Keyed - A Simple Password Generator"),
+
+			app.Div().Style("padding-bottom", "10px").Body(
+				
+				app.Div().Body(
+					app.Text("Length: "),
+					app.Input().Type("number").Min(1).Max(30).Value(16).ID("LENGTH_NUMBER").Style("margin-right", "5px"),
+					app.Input().Type("range").Min(1).Max(30).Value(16).ID("LENGTH_SLIDER"),
+				),
+			),
 
 			app.Div().Class("checkbox-container").Body(
 				app.Input().Class("checkbox").Type("checkbox").ID("CH0"),
@@ -32,29 +40,33 @@ func (k *keyed) Render() app.UI {
 			app.Div().Class("checkbox-container").Body(
 				app.Input().Type("checkbox").Type("checkbox").ID("CH2"),
 				app.Label().For("CH2").Body(
-					app.Text("Include Special Characters ('&', '*', '+', '/')"),
+					app.Text("Include Special Characters ('&', '*', '+', '/', ...)"),
 				),
 			),
 
 			app.Div().Class("checkbox-container").Body(
 				app.Input().Type("checkbox").Type("checkbox").ID("CH3"),
 				app.Label().For("CH3").Body(
-					app.Text("Exclude Ambiguous Characters ('l', 'L', 'O', '1')"),
+					app.Text("Exclude Ambiguous Characters ('l', 'L', 'O', '1', ...)"),
 				),
 			),
 
 			app.Div().Body(
 				app.Form().Body(
-					app.Label().Body(app.Text("Password")),
-					app.Input().
-						Type("text"),
+					app.Label().Body(app.Text("Password: ")),
+					app.Input().Type("text").Style("margin-right", "5px"),
+					app.Button().OnClick(doNothing).Body(app.Text("Copy")),
 				),
 			),
+
 			app.Div().Body(
 				app.Button().Body(app.Text("Generate")),
-				app.Button().Body(app.Text("Copy")),
 			),
 		)
+}
+
+func doNothing(src app.EventHandler, e app.Event) {
+
 }
 
 func main() {
@@ -68,7 +80,6 @@ func main() {
 		Name:        "Keyed",
 		Description: "A Simple Password Generator",
 		Styles: []string{
-			// "https://unpkg.com/spectre.css",
 			"/web/styles.css",
 		},
 	})
