78a9601d0717ecf747f082a5126e2eee4fda5e41
James Moreau <43487194+JamesMoreau@users.noreply.github.com>
2023-06-11 22:25:37 -0400

    .

--- main.go
+++ main.go
@@ -6,57 +6,69 @@ import (
 	"net/http"
 )
 
-// hello is a component that displays a simple "Hello World!". A component is a
-// customizable, independent, and reusable UI element. It is created by
-// embedding app.Compo into a struct.
-type hello struct {
+type keyed struct {
 	app.Compo
 }
 
-func (h *hello) Render() app.UI {
-	return app.Div().Body(
-		app.H1().
-			Class("title").
-			Text("Keyed - A Simple Password Generator"),
-		app.Div().Body(
-			app.Input().
-				Type("checkbox"),
-			app.Text("Include Uppercase Letters"),
-		),
-		app.Div().Body(
-			app.Input().
-				Type("checkbox"),
-			app.Text("Include Digits"),
-		),
-		app.Div().Body(
-			app.Input().
-				Type("checkbox"),
-			app.Text("Include Special Characters ('&', '*', '+', '/')"),
-		),
-		app.Div().Body(
-			app.Input().
-				Type("checkbox"),
-			app.Text("Exclude Ambiguous Characters ('l', 'L', 'O', '1', ...)"),
-		),
-		app.Div().Body(
-			app.Form().Body(
-				app.Label().Body(app.Text("Password")),
+func (k *keyed) Render() app.UI {
+	return app.Div().
+		Style("width", "400px").
+		Style("height", "200px").
+		Style("background-color", "deepskyblue").
+		Body(
+			app.H1().
+				Class("title").
+				Text("Keyed - A Simple Password Generator"),
+			app.Div().Body(
 				app.Input().
-					Type("text"),
+					Type("checkbox"),
+				app.Text("Include Uppercase Letters"),
 			),
-			app.Button().Body(app.Text("Copy")),
-		),
-	)
+			app.Div().
+				Class("checkbox").
+				Style("background-color", "brown").
+				Body(
+					app.Input().
+						Type("checkbox"),
+					app.Text("Include Digits"),
+				),
+			app.Div().Body(
+				app.Input().
+					Type("checkbox"),
+				app.Text("Include Special Characters ('&', '*', '+', '/')"),
+			),
+			app.Div().Body(
+				app.Input().
+					Type("checkbox"),
+				app.Text("Exclude Ambiguous Characters ('l', 'L', 'O', '1', ...)"),
+			),
+			app.Div().Body(
+				app.Form().Body(
+					app.Label().Body(app.Text("Password")),
+					app.Input().
+						Type("text"),
+				),
+			),
+			app.Div().Body(
+				app.Button().Body(app.Text("Generate")),
+				app.Button().Body(app.Text("Copy")),
+			),
+		)
 }
 
 func main() {
-	app.Route("/", &hello{})
+	app.Route("/", &keyed{})
 
 	app.RunWhenOnBrowser()
 
 	http.Handle("/", &app.Handler{
-		Name:        "Hello",
-		Description: "An Hello World! example",
+		Author:      "James Moreau",
+		Version:     "1.0",
+		Name:        "Keyed",
+		Description: "A Simple Password Generator",
+		Styles: []string{
+			"/web/styles.css",
+		},
 	})
 
 	if err := http.ListenAndServe(":8000", nil); err != nil {
