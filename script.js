578ff07bb32460774606eebed095765b46c86c6d
James Moreau <43487194+JamesMoreau@users.noreply.github.com>
2023-06-13 13:45:41 -0400

    update.

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+function updateNumberValue(value) {
+	document.getElementById('lengthNumberInput').value = value;
+}
+
+function updateRangeValue(value) {
+	document.getElementById('lengthRangeInput').value = value;
+}
+
+function copyPassword() {
+	passwordInput = document.getElementById('PASS')
+
+	password = passwordInput.value
+	navigator.clipboard.writeText(password)
+}
+
+// WASM stuff
+
 const goWasm = new Go()
 
 WebAssembly.instantiateStreaming(fetch('main.wasm'), goWasm.importObject)
-	.then((result) => {
-		goWasm.run(result.instance)
-	})
+.then((result) => {
+	goWasm.run(result.instance)
+})
 
 function generatePasswordFromInput() {
-	length = document.getElementById('LENGTH_NUMBER').value
+	length = document.getElementById('lengthNumberInput').value
 	includeUppercaseLettersInput = document.getElementById('CB0').checked
 	includeDigitsInput = document.getElementById('CB1').checked
 	includeSpecialCharactersInput = document.getElementById('CB2').checked
@@ -15,11 +32,4 @@ function generatePasswordFromInput() {
 	passwordInput = document.getElementById('PASS')
 	password = generatePassword(length, includeUppercaseLettersInput, includeDigitsInput, includeSpecialCharactersInput, excludeAmbiguousCharactersInput)
 	passwordInput.value = password
-}
-
-function copyPassword() {
-	passwordInput = document.getElementById('PASS')
-
-	password = passwordInput.value
-	navigator.clipboard.writeText(password)
 }
